b458f19d316e514ab9d958109d3449a8a6d64f5e
hachim-soutra <hachimsoutra10@gmail.com>
2024-04-14 18:00:08 +0200

    add InviteCreatedUser

---
+++ src/Notifications/InviteCreatedUser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ExpertShipping\Spl\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InviteCreatedUser extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->queue = 'notifications';
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Your manager invited your to activate your account.')
+                    ->action('Activate my Account', url("/activate-account/{$notifiable->activate_account_token}"))
+                    ->line('Thank you!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
