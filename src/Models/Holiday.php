0b0bce60a130e3fc3d610b42b3f2bfb5d3aa8d47
h.ibrghoutn <hamza.ibrghoutn@gmail.com>
2024-08-14 19:22:47 +0100

    reseler module holiday update

--- src/Models/Holiday.php
+++ src/Models/Holiday.php
@@ -9,7 +9,11 @@ class Holiday extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     protected $casts = [
-        'date' => 'datetime:Y-m-d'
+        'date'          => 'datetime:Y-m-d',
+        'date_from'     => 'datetime:Y-m-d',
+        'date_to'       => 'datetime:Y-m-d',
     ];
 }
