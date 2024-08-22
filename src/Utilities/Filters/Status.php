cefb27b072c5c5e8488b12c182463b183bdb65f9
Mouad Aarab <30688791+mouadaarab@users.noreply.github.com>
2024-08-22 15:39:50 +0100

    Update Status.php

--- src/Utilities/Filters/Status.php
+++ src/Utilities/Filters/Status.php
@@ -8,6 +8,30 @@ class Status implements FilterContract
 {
     public static function apply($query, $value)
     {
+        if(get_class($query->getModel()) === 'App\Invoice'){
+            if($value === 'paid'){
+                return $query->whereNotNull('paid_at');
+            }
+
+            if($value === 'unpaid'){
+                return $query->whereNull('paid_at');
+            }
+
+            if($value === 'refunded'){
+                return $query->whereNotNull('refunded_at');
+            }
+        }
+
+        if(get_class($query->getModel()) === 'App\Shipment'){
+            return $query->where('type', $value);
+        }
+
+        if(get_class($query->getModel()) === 'App\Insurance'){
+            return $query->whereHas('shipment', function ($query) use ($value) {
+                $query->where('type', $value);
+            });
+        }
+        
         return $query->where('status', $value);
     }
 }
