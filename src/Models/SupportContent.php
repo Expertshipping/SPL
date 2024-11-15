792412ddc8c665cfcdf0def5eee5c4488e5da5b6
hachim-soutra <hachimsoutra10@gmail.com>
2024-11-15 14:20:26 +0100

    add readme

--- src/Models/SupportContent.php
+++ src/Models/SupportContent.php
@@ -2,8 +2,10 @@
 
 namespace ExpertShipping\Spl\Models;
 
+use ExpertShipping\Spl\Enum\ProfileType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class SupportContent extends Model
 {
@@ -14,5 +16,26 @@ class SupportContent extends Model
     public function supportCategory()
     {
         return $this->belongsTo(SupportCategory::class);
+    }
+
+    protected $casts = [
+        'profiles' => 'array',
+    ];
+
+    public function setProfilesAttribute(array $profiles)
+    {
+        $this->attributes['profiles'] = json_encode(
+            array_map(fn($profile) => $profile instanceof ProfileType ? $profile->value : $profile, $profiles)
+        );
+    }
+
+    public function getProfilesAttribute($value)
+    {
+        return array_map(fn($profile) => ProfileType::tryFrom($profile), json_decode($value, true) ?? []);
+    }
+
+    public function scopeWhereProfile(Builder $query, ProfileType $profileType): Builder
+    {
+        return $query->whereJsonContains('profiles', $profileType->value);
     }
 }
