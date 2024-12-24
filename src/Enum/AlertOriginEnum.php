533c6cc71df45a2e2292dd60b1f4c57b6662aff3
hachim-soutra <hachimsoutra10@gmail.com>
2024-12-24 10:49:33 +0100

    update Alert.php

---
+++ src/Enum/AlertOriginEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ExpertShipping\Spl\Enum;
+
+enum AlertOriginEnum:string {
+    case B2B = 'B2B';
+    case RE_SELLER = 'RE_SELLER';
+    case RE_SELLER_MANAGER = 'RE_SELLER_MANAGER';
+}
