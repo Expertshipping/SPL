5888164c8f9cfc0b7d7e426db1600c12d0ec93c7
Mouad Aarab <30688791+mouadaarab@users.noreply.github.com>
2025-01-07 17:31:58 +0100

    Update LocalInvoice.php

--- src/Models/LocalInvoice.php
+++ src/Models/LocalInvoice.php
@@ -856,7 +856,10 @@ class LocalInvoice extends Model
     public function generateInvoiceNumber()
     {
         $year = now()->year;
-        $number = self::query()->whereYear('created_at', now()->year)->count();
+        $number = self::query()
+            ->withTrashed()
+            ->whereYear('created_at', now()->year)
+            ->count();
         return $year . '-' . str_pad($number, 6, '0', STR_PAD_LEFT);
     }
 }
