76f4065074a6488046229c1e46d579d950016fcc
Mouad Aarab <30688791+mouadaarab@users.noreply.github.com>
2025-06-05 16:16:03 +0100

    Update RemoteAreaDelivery to include city in postal code check

--- src/Services/RemoteAreaDelivery.php
+++ src/Services/RemoteAreaDelivery.php
@@ -3,14 +3,18 @@
 namespace ExpertShipping\Spl\Services;
 
 use ExpertShipping\Spl\Models\RemoteArea;
+use Illuminate\Database\Eloquent\Builder;
 
 class RemoteAreaDelivery
 {
-    public static function check(string $country, string $postalCode)
+    public static function check(string $country, string $postalCode, string $city)
     {
         return RemoteArea::query()
             ->where('country', $country)
-            ->where('postal_code', $postalCode)
+            ->where(function (Builder $query) use ($postalCode, $city) {
+                $query->where('postal_code', $postalCode)
+                    ->orWhere('postal_code', $city);
+            })
             ->get()
             ->map(fn($r) => $r->carrier->slug ?? 'ALL')
             ->unique()
