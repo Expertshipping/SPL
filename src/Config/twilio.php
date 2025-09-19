c3035c44f69445bd080a1eec5deb0431b8ca387c
Mouad Aarab <30688791+mouadaarab@users.noreply.github.com>
2025-09-19 16:38:33 +0100

    Add Twilio SMS integration and service class
    Added the twilio/sdk dependency to composer.json. Introduced a new configuration file for Twilio credentials and implemented SmsService for sending SMS and MMS messages using Twilio, including environment and phone validation checks.

---
+++ src/Config/twilio.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'sid'  => env('TWILIO_SID', 'AC52cef60f6627431d013f2c97512e9592'),
+    'auth_token'  => env('TWILIO_AUTH_TOKEN', '0afe772bb8612d49b0cee76c8ee5ccc6'),
+    'number'  => env('TWILIO_NUMBER', '+14503901006'),
+];
